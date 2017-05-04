590f03952488154e3f5d9194f05260577a89da12
agumura <arumuga.ganesan@outlook.com>
2017-05-03 21:31:44 -0400

    Add files via upload

--- testtt.py
+++ testtt.py
@@ -2,14 +2,15 @@ import cv2
 import numpy as np
 import copy
 import matplotlib.pyplot as plt
+import math
 from PIL import Image
 #from scipy import ndimage
 
-def simpleBlobDetector(ImageName, x, y) :
+def simpleBlobDetector(ImageName) :
     im = cv2.imread(ImageName, cv2.IMREAD_GRAYSCALE)
     width, height = im.shape
 
-    #dst = copy.copy(im)
+    dst = copy.copy(im)
     
 
     im[im >= 128] = 255
@@ -22,16 +23,36 @@ def simpleBlobDetector(ImageName, x, y) :
     
     #cv2.resize(im, dst, dst.size(), 0, 0, cv2.INTER_AREA)
     #cv2.resize(im, dst, dst.size(), 0, 0, cv2.INTER_AREA)
-    resized_image = cv2.resize(im, (x, y)) 
-    dst = cv2.resize(resized_image, (height, width))
-        
+    #resized_image = cv2.resize(im, (x, y)) 
+    #dst = cv2.resize(resized_image, (height, width)
+    pixelSize = 20
+
+    
+    image = Image.open(ImageName).convert("L")
+    
+    a = math.floor(image.size[0]/pixelSize)
+    b = math.floor(image.size[1]/pixelSize)   
+    
+    image = image.resize((a, b), Image.NEAREST)
+    image = image.resize((image.size[0]*pixelSize, image.size[1]*pixelSize), Image.NEAREST)    
+
+    image = image.resize((a/2, b/2), Image.NEAREST)
+    image = image.resize((image.size[0]*pixelSize, image.size[1]*pixelSize), Image.NEAREST)    
+   
+    dst = np.array(image)
+    
+    #data = np.asarray( image, dtype="int32" )
+    #img = Image.fromarray( np.asarray( np.clip(npdata,0,255), dtype="uint8"), "L" )  
+    #dst = cv2.imread(data)
+    
+    
     #img = img.resize((20,20),Image.ANTIALIAS) # downsize
     #img = img.resize((369,472), Image.ANTIALIAS) # upsize     
     
+    a = 60
     
-    
-    dst[dst >= 128] = 255
-    dst[dst < 128] = 0   
+    dst[dst >= a] = 255
+    dst[dst < a] = 0   
     
     #plt.imshow(dst, cmap='gray', interpolation='bicubic')
     #plt.plot([], []) 
@@ -59,7 +80,10 @@ def simpleBlobDetector(ImageName, x, y) :
     
     params.filterByConvexity = True
     params.minConvexity = 0
-    params.maxConvexity = 1      
+    params.maxConvexity = 1     
+    
+    #params.filterByArea = True
+    #params.minArea = 40
     
     detector2 = cv2.SimpleBlobDetector_create(params)  
     keypoints2 = detector2.detect(dst)
@@ -69,9 +93,7 @@ def simpleBlobDetector(ImageName, x, y) :
     #imf = cv2.imread(dst, cv2.IMREAD_GRAYSCALE)
     
     im_with_keypoints = cv2.drawKeypoints(dst, keypoints, np.array([]), (255,99,71), 0)
-    
-    
-    
+       
     plt.imshow(im_with_keypoints, cmap='gray', interpolation='bicubic')
     plt.plot([], []) 
     plt.show()    
@@ -92,5 +114,24 @@ def downsize(ImageName):
     #img.save(newSourceFile, quality = 95, dpi=(72,72), optimize = True) 
     
     plt.imshow(img, cmap='gray', interpolation='bicubic')
+    plt.plot([], []) 
+    plt.show()        
+
+def downsize2(ImageName):
+    backgroundColor = (0,)*3
+    pixelSize = 15
+    
+    image = Image.open(ImageName)
+    image = image.resize((image.size[0]/pixelSize, image.size[1]/pixelSize), Image.NEAREST)
+    image = image.resize((image.size[0]*pixelSize, image.size[1]*pixelSize), Image.NEAREST)
+    pixel = image.load()
+    
+    """for i in range(0,image.size[0],pixelSize):
+        for j in range(0,image.size[1],pixelSize):
+            for r in range(pixelSize):
+                pixel[i+r,j] = backgroundColor
+                pixel[i,j+r] = backgroundColor"""
+    
+    plt.imshow(image, cmap='gray', interpolation='bicubic')
     plt.plot([], []) 
     plt.show()        
